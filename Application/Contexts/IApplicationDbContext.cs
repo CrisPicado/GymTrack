00a47a6668b528d1b0ed0b538e7726bd2fb53a1c
KennethGodi1311 <106417438+KennethGodi1311@users.noreply.github.com>
2024-03-06 20:29:15 -0600

    codigo de aplicacion
    cambios y entidades de la base de datos de aplicacion

---
+++ Application/Contexts/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contexts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Entrenador> Entrenador { get; set; }
+        DbSet<Rutina> Rutina { get; set; }
+        DbSet<Usuarios> Usuarios { get; set; }
+        DbSet<Ejercicio> Ejercicios { get; set; }
+        void Save();
+    }
+}
