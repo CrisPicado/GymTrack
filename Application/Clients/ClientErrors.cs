cf795b6d26b6c1dd20bf98750c4746aed0f73def
Cristopher Picado <cpicado869@gmail.com>
2024-03-18 22:10:37 -0600

    Se adicionan las clases de "Client" al Application, se crea clase Injection con las dependencias para "Client" y "Coach" y se cargan al program.cs.

---
+++ Application/Clients/ClientErrors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Application.Clients
+{
+    public class ClientErrors
+    {
+        public static Error NotFound(int id) =>
+            new Error("Clients.NOT_FOUND", $"The client with id {id} was not found.");
+
+        public static Error NotFound() =>
+            new Error("Clients.NOT_FOUND", $"The client was not found.");
+    }
+}
---
+++ Application/Injection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Clients;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class Injection
+    {
+        public static IServiceCollection AddApplicationServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IClientService, ClientService>();
+            //services.AddScoped<ICoachService, CoachService>();
+            services.AddAutoMapper(typeof(ClientProfile));
+            //services.AddAutoMapper(typeof(CoachProfile));
+
+            return services;
+        }
+    }
+}
